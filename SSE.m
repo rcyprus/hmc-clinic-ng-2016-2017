86fa2054c65521558eb5b3f2ae96242c001ad951
Paige Rinnert <prinnert@g.hmc.edu>
2016-10-09 21:21:05 -0700

    updated SSE algorithm

--- SSE.m
+++ SSE.m
@@ -17,11 +17,7 @@ Y = [y0 y1 y2]; % Y has as size sensors outputs p by timesteps T
 %% Actual algorithm
 cvx_begin
 	variable x(n) % x is a variable with length n (number of states)
-    minimize( norm(Y - [C*x C*A*x C*(A^2)*x], Inf) )
-%	minimize( sum(norms(Y - [C*x C*A*x C*(A^2)*x], 10, 2)) ) % right kind of norm?
-%       I think the solution should be something like the line above but it
-%       doesn't work right now :(
-%       Maybe the norms function doesn't do what I think it does
+	minimize( sum(norms(Y - [C*x C*A*x C*(A^2)*x], Inf, 2)) ) % right kind of norm?
 	subject to
 		isreal(x)
 cvx_end
